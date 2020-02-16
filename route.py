f7987d4853b7981404393cc04f1993432f94903d
AustinOldham <tech@oldham.cc>
2020-02-15 23:07:05 -0500

    Added placeholder routes

--- route.py
+++ route.py
@@ -2,5 +2,16 @@ from main import app
 
 @app.route('/')
 def hello_world():
-	return 'Modular test'
+	return "Modular test"
 
+@app.route('/signup')
+def signup():
+	return "Sign up placeholder"
+
+@app.route('/signin')
+def signin():
+	return "Sign in placeholder"
+
+@app.route('/profile/<username>')
+def profile_page(username):
+	return "Placeholder for {}".format(username)
