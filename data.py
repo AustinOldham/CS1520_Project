3446c47f9e90f6dcfff8a8c0f8a6f4352c0219e3
Austin Oldham <alo52@pitt.edu>
2020-02-20 12:21:03 -0500

    Added code for signing up

---
+++ data.py
@@ -0,0 +1,123 @@
+from google.cloud import datastore
+
+import hashlib
+
+# This code is based on the code found at https://github.com/timothyrjames/cs1520 with permission from the instructor
+
+_PROJECT_ID = 'roommate-tinder'
+_USER_ENTITY = 'user'
+
+
+class User(object):
+    def __init__(self, username, email, about=''):
+        self.username = username
+        self.email = email
+        self.about = about
+
+    def to_dict(self):
+        return {
+            'username': self.username,
+            'about': self.about,
+            'email': self.email,
+        }
+
+
+def _get_client():
+    """Returns the datastore client"""
+
+    return datastore.Client(_PROJECT_ID)
+
+
+def _load_key(client, entity_type, entity_id=None, parent_key=None):
+    """Load a datastore key using a particular client, and if known, the ID.  Note
+    that the ID should be an int - we're allowing datastore to generate them in
+    this example."""
+
+    key = None
+    if entity_id:
+        key = client.key(entity_type, entity_id, parent=parent_key)
+    else:
+        # this will generate an ID
+        key = client.key(entity_type)
+    return key
+
+
+def _load_entity(client, entity_type, entity_id, parent_key=None):
+    """Load a datstore entity using a particular client, and the ID."""
+
+    key = _load_key(client, entity_type, entity_id, parent_key)
+    entity = client.get(key)
+    # log('retrieved entity for ' + str(entity_id))
+    return entity
+
+
+def load_user(username, passwordhash):
+    """Load a user based on the passwordhash; if the passwordhash doesn't match
+    the username, then this should return None."""
+
+    client = _get_client()
+    q = client.query(kind=_USER_ENTITY)
+    q.add_filter('username', '=', username)
+    q.add_filter('passwordhash', '=', passwordhash)
+    for user in q.fetch():
+        return User(user['username'], user['email'], user['about'])
+    return None
+
+
+def load_about_user(username):
+    """Return a string that represents the "About Me" information a user has
+    stored."""
+
+    user = _load_entity(_get_client(), _USER_ENTITY, username)
+    if user:
+        return user['about']
+    else:
+        return ''
+
+
+def save_user(user, passwordhash):
+    """Save the user details to the datastore."""
+
+    client = _get_client()
+    entity = datastore.Entity(_load_key(client, _USER_ENTITY, user.username))
+    entity['username'] = user.username
+    entity['email'] = user.email
+    entity['passwordhash'] = passwordhash
+    entity['about'] = ''
+    entity['completions'] = []
+    client.put(entity)
+
+
+def save_about_user(username, about):
+    """Save the user's about info to the datastore."""
+
+    client = _get_client()
+    user = _load_entity(client, _USER_ENTITY, username)
+    user['about'] = about
+    client.put(user)
+
+
+def create_data():
+    """You can use this function to populate the datastore with some basic
+    data."""
+
+    client = _get_client()
+    entity = datastore.Entity(client.key(_USER_ENTITY, 'testuser'),
+                              exclude_from_indexes=[])
+    entity.update({
+        'username': 'testuser',
+        'passwordhash': '',
+        'email': '',
+        'about': '',
+        'completions': [],
+    })
+    client.put(entity)
+
+
+def get_password_hash(pw):
+    """This will give us a hashed password that will be extremlely difficult to
+    reverse.  Creating this as a separate function allows us to perform this
+    operation consistently every time we use it."""
+
+    encoded = pw.encode('utf-8')
+    return hashlib.sha256(encoded).hexdigest()
--- route.py
+++ route.py
@@ -1,19 +1,51 @@
-from flask import render_template
+from flask import render_template, request, session, redirect
+from email.utils import parseaddr
 from main import app
+import data
+
+# Part of this code is based on the code found at https://github.com/timothyrjames/cs1520 with permission from the instructor
+
 
 @app.route('/')
 @app.route('/homepage.html')
 def root():
-  return render_template('homepage.html')
+	return render_template('homepage.html')
+
 
 @app.route('/signup.html')
 def signup():
-    return render_template('signup.html')
+	return render_template('signup.html')
+
 
 @app.route('/signin')
 def signin():
 	return "Sign in placeholder"
 
+
 @app.route('/profile/<username>')
 def profile_page(username):
 	return "Placeholder for {}".format(username)
+
+
+@app.route('/register', methods=['POST'])
+def register_user():
+	username = request.form.get('username')
+	password1 = request.form.get('password1')
+	password2 = request.form.get('password2')
+	email = request.form.get('email')
+	errors = []
+	if password1 != password2:
+		errors.append('Passwords do not match.')
+	email_parts = parseaddr(email)
+	if len(email_parts) != 2 or not email_parts[1]:
+		errors.append('Invalid email address: ' + str(email))
+	user = data.User(username, email)
+	if errors:
+		# return show_page('/signup.html', 'Sign Up', errors=errors)
+		pass
+	else:
+		passwordhash = data.get_password_hash(password1)
+		data.save_user(user, passwordhash)
+		session['user'] = user.username
+		return redirect('/profile/<username>')
+
