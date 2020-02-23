f5fd7401ec38079dd1d840750fd86a821cac665d
Austin Oldham <alo52@pitt.edu>
2020-02-22 22:08:57 -0500

    Added unlike_js()

--- static/js/like_unlike.js
+++ static/js/like_unlike.js
@@ -7,3 +7,13 @@ function like_js(other_username) {
 		}
 	}
 }
+
+function unlike_js(other_username) {
+	$.ajax({url: "/unlikeuser/" + other_username, success: function(result){
+		if (result == 'success') {
+			$("#like_unlike_button").text("Like");
+			$("#like_unlike_button").attr("onclick", "like_js(" + other_username + ")")
+			$("#like_unlike_button").attr("class", "btn btn-primary")
+		}
+	}
+}
