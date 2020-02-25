7ab8f8d576d55aa0ff5a7d17ce3e4f1d9c45e624
Austin Oldham <alo52@pitt.edu>
2020-02-24 22:56:45 -0500

    Fixed lack of quotation marks in liked_unlike.js.

--- static/js/like_unlike.js
+++ static/js/like_unlike.js
@@ -2,7 +2,7 @@ function like_js(other_username) {
 	$.ajax({url: "/likeuser/" + other_username, success: function(result){
 		if (result == 'success') {
 			$("#like_unlike_button").text("Unlike");
-			$("#like_unlike_button").attr("onclick", "unlike_js(" + other_username + ")")
+			$("#like_unlike_button").attr("onclick", "unlike_js(\"" + other_username + "\")")
 			$("#like_unlike_button").attr("class", "btn btn-warning")
 		}
 	}});
@@ -12,7 +12,7 @@ function unlike_js(other_username) {
 	$.ajax({url: "/unlikeuser/" + other_username, success: function(result){
 		if (result == 'success') {
 			$("#like_unlike_button").text("Like");
-			$("#like_unlike_button").attr("onclick", "like_js(" + other_username + ")")
+			$("#like_unlike_button").attr("onclick", "like_js(\"" + other_username + "\")")
 			$("#like_unlike_button").attr("class", "btn btn-primary")
 		}
 	}});
