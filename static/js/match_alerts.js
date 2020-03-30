da5381081b337c1e073d85be9f913cfdef5f2e19
AustinOldham <tech@oldham.cc>
2020-03-30 16:23:13 -0500

    Started writing a script to alert users when they match with a new person.

---
+++ static/js/match_alerts.js
@@ -0,0 +1,11 @@
+function saveAlerted() {
+	//This function stores a cookie that records which matches the user has been alerted about.
+}
+
+function clearUnmatchedRoommates() {
+	//This function removes all recorded roommates that are no longer matched with the user.
+}
+
+function alertUser() {
+	
+}
