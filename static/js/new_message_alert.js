beae8ffe68dfdfdc7e34586c30b8a8a6baa4e54a
jecarter17 <jec214@pitt.edu>
2020-04-22 01:03:55 -0400

    added logging for new messages check

--- static/js/new_message_alert.js
+++ static/js/new_message_alert.js
@@ -1,13 +1,17 @@
 function check_for_new_messages() {
+    console.log("running new messages check");
 	$.ajax({url: "/countMessages", success: function(result){
+        console.log(result);
         var num_messages = parseInt(result);
         var new_messages = num_messages - load_current_message_count();
+        console.log(new_messages);
         alert("You have " + new_messages + " new messages");
         store_new_message_count(result);
 	}});
 }
 
 function load_current_message_count(){
+    console.log(document.cookie);
     return parseInt(document.cookie);
 }
 
